b01d520fe1f4c8f9717cbe1dc760b4323867482e
alfsuace <alfonso.suaace@educa.jcyl.es>
2023-06-13 16:59:45 +0200

    Correcion borja

--- src/domain/useCase/DeleteBankAccountUseCase.java
+++ src/domain/useCase/DeleteBankAccountUseCase.java
@@ -6,8 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DeleteBankAccountUseCase {
-    public void borrarCuentaBancaria(String id) throws SQLException {
-        String consulta = "DELETE FROM cuentaBancaria WHERE id=?";
+    public void borrarCliente(String id) throws SQLException {
+        String consulta = "DELETE FROM customer WHERE id=?";
         Conexion conexion = new Conexion();
         PreparedStatement statement = conexion.prepareStatementFunction(consulta);
         statement.setString(1, id);
@@ -15,5 +15,6 @@ public class DeleteBankAccountUseCase {
         statement.close();
         conexion.cerrarConexion();
     }
+
 
 }
--- src/domain/useCase/ReadCustomerUseCase.java
+++ src/domain/useCase/ReadCustomerUseCase.java
@@ -1,15 +1,46 @@
 package domain.useCase;
 
+import data.Conexion;
 import domain.models.Customer;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadCustomerUseCase {
     private CustomerRepository CustomerRepository;
 
-    public ReadCustomerUseCase(CustomerRepository CustomerRepository){
-        this.CustomerRepository=CustomerRepository;
-    }
+    public List<Customer> obtenerUsuarios() throws SQLException {
+        Conexion conexion = new Conexion();
+        List<Customer> usuarios = new ArrayList<>();
+        String consulta = "SELECT * FROM usuario";
+        PreparedStatement statement = conexion.prepareStatementFunction(consulta);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            String id = resultSet.getString("id");
+            String name = resultSet.getString("name");
+            String surname = resultSet.getString("surname");
+            String dni = resultSet.getString("dni");
+            String address = resultSet.getString("address");
+            String town = resultSet.getString("town");
+            String addressCode = resultSet.getString("addressCode");
 
-    public Customer execute(String codigoCustomer){
-        return CustomerRepository.findById(codigoCustomer);
+            Customer usuario = new Customer();
+            usuario.setId(id);
+            usuario.setName(name);
+            usuario.setSurname(surname);
+            usuario.setDni(dni);
+            usuario.setAddress(address);
+            usuario.setTown(town);
+            usuario.setAddressCode(addressCode);
+
+            usuarios.add(usuario);
+        }
+        resultSet.close();
+        statement.close();
+        conexion.cerrarConexion();
+        return usuarios;
     }
 }
